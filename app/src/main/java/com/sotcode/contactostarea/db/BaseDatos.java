136d47e1cf662de36262f254cbd1b29e283c9ce6
rene2m <rene.mamani.sto@gmail.com>
2020-05-25 12:31:35 -0400

    introduccion de base de datos almacenamiento y  extraccion en SQLite

--- app/src/main/java/com/sotcode/contactostarea/db/BaseDatos.java
+++ app/src/main/java/com/sotcode/contactostarea/db/BaseDatos.java
@@ -8,7 +8,6 @@ import android.content.QuickViewConstants;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import androidx.annotation.Nullable;
 
 import com.sotcode.contactostarea.pojo.Contacto;
